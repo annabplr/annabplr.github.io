8da10160d0408be4ba19f50932b924e20fb69cc3
Annabelle PELLIER <annabplr@gmail.com>
2019-01-27 17:41:44 +0100

    add scroll defilment

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,27 @@ document.querySelectorAll(".resume-each").forEach((button) => {
   event.currentTarget.querySelector('div.resume-hide').classList.toggle("resume-display");
 });
 });
+
+
+// display div on scroll
+
+  $(function () { // wait for document ready
+    // init
+    var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave'
+      }
+    });
+
+    // get all slides
+    var slides = document.querySelectorAll("section.panel");
+
+    // create scene for every slide
+    for (var i=0; i<slides.length; i++) {
+      new ScrollMagic.Scene({
+          triggerElement: slides[i]
+        })
+        .setPin(slides[i])
+        .addTo(controller);
+    }
+  });
