cddefab51be172ea5d5b5158762f094a550f5d41
Annabelle PELLIER <annabplr@gmail.com>
2019-01-29 14:16:11 +0100

    add scroll effect when clicking on a link navbar

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,14 @@ document.querySelector(".lapioche").
     document.querySelector('.detail-terracognita').classList.remove("display-projects");
   document.querySelector('.detail-lapioche').classList.toggle("display-projects");
 });
+
+// scroll effect when clicking on the link
+
+$(document).ready(function() {
+    $('.scroll-effect').on('click', function() { // Au clic sur un élément
+      var page = $(this).attr('href'); // Page cible
+      var speed = 750; // Durée de l'animation (en ms)
+      $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+      return false;
+    });
+  });
