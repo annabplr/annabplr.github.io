15809b8efee2c945cdcf0bbf59ff816b006b5292
Annab <annabplr@gmail.com>
2020-04-16 17:06:32 +0200

    WIP new design

---
+++ App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    COUCOU
+    <!-- <Menu ref="menu" v-if="ableScroll() || mobileDisplay()" :position= "scrollPosition" :previousPosition= "previousscrollPosition" v-on:navbarNavigation="navbarNavigation" /> -->
+    <!-- <transition :name="transitionName">
+      <router-view :firstLoad="createdApp" :lastPosition="scrollPosition"></router-view>
+    </transition> -->
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'app',
+  methods: {
+  },
+  created () {
+    this.createdApp = true;
+  },
+  mounted () {
+  },
+  beforeUpdate () {
+  },
+  updated() {
+  },
+  destroyed () {
+  },
+  data: function() {
+    return {
+      transitionName: "normal",
+      previousscrollPosition: 0,
+      scrollPosition: 0,
+      maxScrollPosition: 8,
+      isScrolling: false,
+      displayArrow: true,
+      createdApp: false
+    };
+  },
+}
+</script>
+
+<style lang="scss">
+// @import './variables';
+
+
+</style>
