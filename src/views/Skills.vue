8b24fdde7e69f11ec069b06972b2b49555b73624
Annab <annabplr@gmail.com>
2020-04-20 18:00:15 +0200

    add html + css for skills and summary - WIP

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,34 +1,174 @@
 <template>
-  <div class="Skills">
-    COUCOU
-    SKILLLLLLLLLLLLLLLLL
+  <div class="skills">
+    <h2 class="">{{ $t('title') }}</h2>
+    <div class="skills-container">
+      <div class="">
+          <h3>
+            {{ $t('technical-title') }}
+          </h3>
+        <div v-for="item in tech" v-bind:key="item">
+          {{ $t('technical.' + item) }}
+        </div>
+      </div>
+      <div class="">
+        <h3>
+          {{ $t('general-title') }}
+        </h3>
+        <div v-for="item in general" v-bind:key="item">
+          {{ $t('general.' + item) }}
+        </div>
+      </div>
+      <div class="">
+        <h3>
+          {{ $t('perso-title') }}
+        </h3>
+        <div v-for="item in perso" v-bind:key="item">
+          {{ $t('perso.' + item) }}
+        </div>
+      </div>
+      <div class="">
+        <h3>
+          {{ $t('like-title') }}
+        </h3>
+        <div v-for="item in like" v-bind:key="item">
+          {{ $t('like.' + item) }}
+        </div>
+      </div>
+      <div class="">
+        <h3>
+          {{ $t('passion-title') }}
+        </h3>
+        <div v-for="item in passion" v-bind:key="item">
+          {{ $t('passion.' + item) }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <i18n>
 {
   "fr": {
-    "hello": "hello, je m'appelle annabelle PELLIER,",
-    "job":"et je suis développeuse web full-stack",
-    "place":"à Genève"
+    "title": "compétences",
+    "technical-title": "techniques",
+    "technical": {
+      "ruby": "ruby on rails",
+      "html": "html / css / sass",
+      "vueJS": "vueJS",
+      "git": "git & github",
+      "SQL": "SQL",
+      "API": "API",
+      "PWA": "PWA",
+      "heroku": "heroku",
+      "javascript": "javascript / jquery",
+      "angular": "angular"
+    },
+    "general-title": "transverses",
+    "general": {
+      "slack": "slack",
+      "trello": "trello/asana",
+      "zapier": "zapier",
+      "agile": "méthode agile",
+      "figma": "figma / xd",
+      "stripe": "stripe",
+      "anglais" : "anglais" ,
+      "adobe": "suite adobe",
+      "toggle": "toggle / cloquify"
+    },
+    "perso-title": "savoir-être",
+    "perso": {
+      "curious": "curieuse",
+      "rigorous": "rigoureuse",
+      "autonomous": "autonome"
+    },
+    "like-title": "j'aime",
+    "like": {
+      "think": "me creuser la tête",
+      "minimalism": "les designs minimalistes",
+      "learn": "apprendre de nouvelles choses"
+    },
+    "passion-title": "mais aussi",
+    "passion": {
+      "lettering": "la calligraphie",
+      "hiking": "la randonnée",
+      "gardening": "le jardinage"
+    }
   },
   "en": {
-    "hello": "hi, I'm annabelle PELLIER,",
-    "job":"and I'm a full-stack  web developer",
-    "place":"in Geneva"
+    "title": "skills",
+    "technical-title": "technical",
+    "technical": {
+      "ruby": "ruby on rails",
+      "html": "html / css / sass",
+      "vueJS": "vueJS",
+      "git": "git & github",
+      "SQL": "SQL",
+      "API": "API",
+      "PWA": "PWA",
+      "heroku": "heroku",
+      "javascript": "javascript / jquery",
+      "angular": "angular"
+    },
+    "general-title": "transversal",
+    "general": {
+      "slack": "slack",
+      "trello": "trello/asana",
+      "zapier": "zapier",
+      "agile": "agile method",
+      "figma": "figma / xd",
+      "stripe": "stripe",
+      "anglais" : "english" ,
+      "adobe": "adobe suite",
+      "toggle": "toggle / cloquify"
+    },
+    "perso-title": "personal",
+    "perso": {
+      "curious": "curious",
+      "rigorous": "rigoureuse",
+      "autonomous": "autonome"
+    },
+    "like-title": "j'aime",
+    "like": {
+      "think": "me creuser la tête",
+      "minimalism": "les designs minimalistes",
+      "learn": "apprendre de nouvelles choses"
+    },
+    "passion-title": "mais aussi",
+    "passion": {
+      "lettering": "la calligraphie",
+      "hiking": "la randonnée",
+      "gardening": "le jardinage"
+    }
   }
 }
 </i18n>
 
+"think": "me creuser la tête", "minimalism": "les designs minimalistes", "learn": "apprendre de nouvelles choses"
 
 <script>
 export default {
   name: 'Skills',
+  data: function() {
+    return {
+      tech: ["ruby", "html", "vueJS", "git", "SQL", "API", "PWA", "heroku", "javascript", "angular"],
+      general: ["slack", "trello", "zapier", "agile", "figma", "stripe", "anglais", "adobe", "toggle"],
+      perso: ["curious", "riguourous", "autonomous"],
+      like: ["think", "minimalism", "learn"],
+      passion: ["lettering", "hiking", "gardening"]
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+@import "../assets/_variables.scss";
+.skills {
+  &-container {
+    display: flex;
+    justify-content: space-around;
+  }
+}
 img {
   width: 150px;
 }
